e1d52da8edf1df12dfff199e2300e632ee1dcf23
Todd Carrozzi <carrozzi@gmail.com>
2025-11-25 13:06:23 -0500

    Fix: Use libbpf directly instead of libxdp dispatcher
    The libxdp dispatcher was creating duplicate program/map instances:
    - The dispatcher wraps programs with freplace/ext mechanism
    - This creates separate 'xdp' and 'ext' instances with different maps
    - User-space was reading one set of maps while the kernel updated another
    
    Solution:
    - Use bpf_object__open_file() + bpf_object__load() directly
    - Use bpf_xdp_attach() / bpf_xdp_detach() for XDP attachment
    - Single program instance = single set of maps
    - Counters now work correctly

--- user_prog.c
+++ user_prog.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-/* User-space program to read and display XDP statistics */
+/* User-space program to load XDP program and display statistics */
+/* Uses libbpf directly (no libxdp dispatcher) to avoid duplicate map instances */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,8 +13,6 @@
 #include <inttypes.h>
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
-#include <xdp/libxdp.h>
-#include <xdp/xsk.h>
 #include <linux/if_link.h>
 #include <stdbool.h>
 #include <net/if.h>
@@ -26,11 +25,15 @@
 #include <arpa/inet.h>
 #include <linux/if_packet.h>
 #include <linux/if_ether.h>
+#include <xdp/xsk.h>
 
-#include "../common/common_defines.h"
-#include "../common/common_user_bpf_xdp.h"
-#include "../common/common_params.h"
 #include "xdp_prog.h"
+
+/* Exit codes */
+#define EXIT_OK          0
+#define EXIT_FAIL        1
+#define EXIT_FAIL_OPTION 2
+#define EXIT_FAIL_BPF    3
 
 static volatile bool exiting = false;
 
@@ -60,17 +63,25 @@ static struct xsk_socket_info *xsk_info = NULL;
 static pthread_t xsk_thread;
 static char *output_ifname = NULL;
 
+/* Global state for cleanup */
+static struct bpf_object *bpf_obj = NULL;
+static int input_ifindex = -1;
+static int output_ifindex = -1;
+static __u32 input_xdp_flags = 0;
+static __u32 output_xdp_flags = 0;
+static int output_prog_fd = -1;
+
 static void sig_int(int signo)
 {
 	exiting = true;
 }
 
-static int find_map_fd(struct bpf_object *bpf_obj, const char *mapname)
+static int find_map_fd(struct bpf_object *obj, const char *mapname)
 {
 	struct bpf_map *map;
 	int map_fd = -1;
 
-	map = bpf_object__find_map_by_name(bpf_obj, mapname);
+	map = bpf_object__find_map_by_name(obj, mapname);
 	if (!map) {
 		fprintf(stderr, "ERR: cannot find map by name: %s\n", mapname);
 		goto out;
@@ -81,7 +92,7 @@ out:
 	return map_fd;
 }
 
-static void print_stats(struct bpf_object *bpf_obj)
+static void print_stats(struct bpf_object *obj)
 {
 	struct global_stats global_stats;
 	struct mpls_label_stats label_stats;
@@ -91,8 +102,8 @@ static void print_stats(struct bpf_object *bpf_obj)
 	int err;
 	int map_fd_global, map_fd_mpls;
 	
-	map_fd_global = find_map_fd(bpf_obj, "global_stats_map");
-	map_fd_mpls = find_map_fd(bpf_obj, "mpls_stats_map");
+	map_fd_global = find_map_fd(obj, "global_stats_map");
+	map_fd_mpls = find_map_fd(obj, "mpls_stats_map");
 	
 	if (map_fd_global < 0 || map_fd_mpls < 0) {
 		fprintf(stderr, "ERR: failed to find maps\n");
@@ -171,7 +182,6 @@ static int send_packet_to_interface(const void *data, size_t len, const char *if
 	/* Create raw socket */
 	sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 	if (sock < 0) {
-		fprintf(stderr, "ERR: Failed to create raw socket: %s\n", strerror(errno));
 		return -1;
 	}
 
@@ -180,8 +190,6 @@ static int send_packet_to_interface(const void *data, size_t len, const char *if
 	strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
 	ret = ioctl(sock, SIOCGIFINDEX, &ifr);
 	if (ret < 0) {
-		fprintf(stderr, "ERR: Failed to get interface index for %s: %s\n",
-			ifname, strerror(errno));
 		close(sock);
 		return -1;
 	}
@@ -197,12 +205,7 @@ static int send_packet_to_interface(const void *data, size_t len, const char *if
 	ret = sendto(sock, data, len, 0, (struct sockaddr *)&saddr, sizeof(saddr));
 	close(sock);
 
-	if (ret < 0) {
-		fprintf(stderr, "ERR: Failed to send packet: %s\n", strerror(errno));
-		return -1;
-	}
-
-	return 0;
+	return (ret < 0) ? -1 : 0;
 }
 
 static void *xsk_packet_reader(void *arg)
@@ -229,7 +232,7 @@ static void *xsk_packet_reader(void *arg)
 			uint32_t free_frames = xsk->umem_frame_free;
 			if (free_frames > 0) {
 				ret = xsk_ring_prod__reserve(&xsk->umem->fq, free_frames, &idx_fq);
-				if (ret == free_frames) {
+				if (ret == (int)free_frames) {
 					for (uint32_t i = 0; i < free_frames; i++)
 						*xsk_ring_prod__fill_addr(&xsk->umem->fq, idx_fq++) =
 							xsk_alloc_umem_frame(xsk);
@@ -247,11 +250,7 @@ static void *xsk_packet_reader(void *arg)
 
 			/* Send packet to output interface */
 			if (output_ifname) {
-				if (send_packet_to_interface(pkt, len, output_ifname) == 0) {
-					/* Packet sent successfully */
-				} else {
-					fprintf(stderr, "WARNING: Failed to send packet of %u bytes\n", len);
-				}
+				send_packet_to_interface(pkt, len, output_ifname);
 			}
 
 			/* Free the frame back to the umem */
@@ -264,7 +263,7 @@ static void *xsk_packet_reader(void *arg)
 		uint32_t free_frames = xsk->umem_frame_free;
 		if (free_frames > 0) {
 			ret = xsk_ring_prod__reserve(&xsk->umem->fq, free_frames, &idx_fq);
-			if (ret == free_frames) {
+			if (ret == (int)free_frames) {
 				for (uint32_t i = 0; i < free_frames; i++)
 					*xsk_ring_prod__fill_addr(&xsk->umem->fq, idx_fq++) =
 						xsk_alloc_umem_frame(xsk);
@@ -276,7 +275,7 @@ static void *xsk_packet_reader(void *arg)
 	return NULL;
 }
 
-static int setup_af_xdp_socket(const char *ifname, int ifindex, struct bpf_object *bpf_obj, const char *out_ifname)
+static int setup_af_xdp_socket(const char *ifname, int ifindex, struct bpf_object *obj)
 {
 	struct xsk_umem_info *umem;
 	struct xsk_umem_config umem_cfg = {
@@ -289,7 +288,7 @@ static int setup_af_xdp_socket(const char *ifname, int ifindex, struct bpf_objec
 	struct xsk_socket_config xsk_cfg = {
 		.rx_size = XSK_RING_CONS__DEFAULT_NUM_DESCS,
 		.tx_size = XSK_RING_PROD__DEFAULT_NUM_DESCS,
-		.libbpf_flags = 0,
+		.libbpf_flags = XSK_LIBBPF_FLAGS__INHIBIT_PROG_LOAD, /* Don't load another XDP prog */
 		.xdp_flags = 0,
 		.bind_flags = XDP_USE_NEED_WAKEUP,
 	};
@@ -346,11 +345,12 @@ static int setup_af_xdp_socket(const char *ifname, int ifindex, struct bpf_objec
 		free(xsk_info);
 		free(umem);
 		munmap(umem_buffer, NUM_FRAMES * FRAME_SIZE);
+		xsk_info = NULL;
 		return -1;
 	}
 
 	/* Get xsks_map file descriptor */
-	map_fd_xsks = find_map_fd(bpf_obj, "xsks_map");
+	map_fd_xsks = find_map_fd(obj, "xsks_map");
 	if (map_fd_xsks < 0) {
 		fprintf(stderr, "ERR: Failed to find xsks_map\n");
 		xsk_socket__delete(xsk_info->xsk);
@@ -358,6 +358,7 @@ static int setup_af_xdp_socket(const char *ifname, int ifindex, struct bpf_objec
 		free(xsk_info);
 		free(umem);
 		munmap(umem_buffer, NUM_FRAMES * FRAME_SIZE);
+		xsk_info = NULL;
 		return -1;
 	}
 
@@ -370,6 +371,7 @@ static int setup_af_xdp_socket(const char *ifname, int ifindex, struct bpf_objec
 		free(xsk_info);
 		free(umem);
 		munmap(umem_buffer, NUM_FRAMES * FRAME_SIZE);
+		xsk_info = NULL;
 		return -1;
 	}
 
@@ -388,20 +390,83 @@ static int setup_af_xdp_socket(const char *ifname, int ifindex, struct bpf_objec
 	return 0;
 }
 
+/* Load BPF object file and get program FD */
+static struct bpf_object *load_bpf_object(const char *filename, const char *prog_name, int *prog_fd)
+{
+	struct bpf_object *obj;
+	struct bpf_program *prog;
+	int err;
+
+	/* Open BPF object file */
+	obj = bpf_object__open_file(filename, NULL);
+	if (libbpf_get_error(obj)) {
+		fprintf(stderr, "ERR: Failed to open BPF object file %s: %s\n",
+			filename, strerror(errno));
+		return NULL;
+	}
+
+	/* Load BPF object into kernel */
+	err = bpf_object__load(obj);
+	if (err) {
+		fprintf(stderr, "ERR: Failed to load BPF object: %s\n", strerror(-err));
+		bpf_object__close(obj);
+		return NULL;
+	}
+
+	/* Find the program by name */
+	prog = bpf_object__find_program_by_name(obj, prog_name);
+	if (!prog) {
+		fprintf(stderr, "ERR: Failed to find program '%s' in object\n", prog_name);
+		bpf_object__close(obj);
+		return NULL;
+	}
+
+	*prog_fd = bpf_program__fd(prog);
+	if (*prog_fd < 0) {
+		fprintf(stderr, "ERR: Failed to get program FD: %s\n", strerror(errno));
+		bpf_object__close(obj);
+		return NULL;
+	}
+
+	return obj;
+}
+
+/* Attach XDP program to interface using libbpf */
+static int attach_xdp(int ifindex, int prog_fd, __u32 *flags_out)
+{
+	int err;
+
+	/* Try native mode first */
+	err = bpf_xdp_attach(ifindex, prog_fd, XDP_FLAGS_DRV_MODE, NULL);
+	if (err == 0) {
+		*flags_out = XDP_FLAGS_DRV_MODE;
+		return 0;
+	}
+
+	/* Fall back to SKB mode */
+	err = bpf_xdp_attach(ifindex, prog_fd, XDP_FLAGS_SKB_MODE, NULL);
+	if (err == 0) {
+		*flags_out = XDP_FLAGS_SKB_MODE;
+		return 0;
+	}
+
+	fprintf(stderr, "ERR: Failed to attach XDP program: %s\n", strerror(-err));
+	return err;
+}
+
+/* Detach XDP program from interface */
+static int detach_xdp(int ifindex, __u32 flags)
+{
+	return bpf_xdp_detach(ifindex, flags, NULL);
+}
+
 int main(int argc, char **argv)
 {
-	struct xdp_program *program = NULL;
-	struct xdp_program *output_program = NULL;
-	struct bpf_object *bpf_obj;
-	struct config cfg = {
-		.ifindex = -1,
-		.do_unload = false,
-		.attach_mode = XDP_MODE_NATIVE,
-	};
-	enum xdp_attach_mode output_attach_mode = XDP_MODE_NATIVE;
 	int err = 0;
 	int interval = 2; /* Default 2 seconds */
-	char errmsg[1024];
+	int prog_fd = -1;
+	char *input_ifname;
+	struct bpf_object *output_obj = NULL;
 	
 	if (argc < 3) {
 		fprintf(stderr, "Usage: %s <input_ifname> <output_ifname> [interval_seconds]\n", argv[0]);
@@ -417,88 +482,55 @@ int main(int argc, char **argv)
 		}
 	}
 	
-	/* Set input interface name */
-	strncpy(cfg.ifname_buf, argv[1], IF_NAMESIZE - 1);
-	cfg.ifname = cfg.ifname_buf;
-	cfg.ifindex = if_nametoindex(cfg.ifname);
-	if (cfg.ifindex == 0) {
+	input_ifname = argv[1];
+	output_ifname = argv[2];
+	
+	/* Get interface indices */
+	input_ifindex = if_nametoindex(input_ifname);
+	if (input_ifindex == 0) {
 		fprintf(stderr, "Failed to get interface index for %s: %s\n",
-			cfg.ifname, strerror(errno));
+			input_ifname, strerror(errno));
 		return EXIT_FAIL_OPTION;
 	}
 	
-	/* Get output interface index */
-	unsigned int output_ifindex = if_nametoindex(argv[2]);
+	output_ifindex = if_nametoindex(output_ifname);
 	if (output_ifindex == 0) {
 		fprintf(stderr, "Failed to get interface index for output interface %s: %s\n",
-			argv[2], strerror(errno));
+			output_ifname, strerror(errno));
 		return EXIT_FAIL_OPTION;
 	}
 	
-	/* Set default BPF-ELF object file and BPF program name */
-	strncpy(cfg.filename, "xdp_prog.o", sizeof(cfg.filename));
-	strncpy(cfg.progname, "xdp_macsec_stats", sizeof(cfg.progname));
-	
-	/* Load and attach BPF program */
-	program = load_bpf_and_xdp_attach(&cfg);
-	if (!program) {
-		fprintf(stderr, "ERR: Failed to load and attach BPF program\n");
+	/* Load and attach main XDP program to input interface */
+	printf("Loading XDP program for input interface %s...\n", input_ifname);
+	bpf_obj = load_bpf_object("xdp_prog.o", "xdp_macsec_stats", &prog_fd);
+	if (!bpf_obj) {
 		return EXIT_FAIL_BPF;
 	}
 	
-	/* Get bpf_object from program */
-	bpf_obj = xdp_program__bpf_obj(program);
-	if (!bpf_obj) {
-		fprintf(stderr, "ERR: Failed to get bpf_object\n");
-		err = EXIT_FAIL_BPF;
-		goto cleanup;
-	}
-	
-	/* Attach a simple XDP_PASS program to output interface (required for redirect) */
-	/* Load it directly so we can handle errors gracefully */
-	DECLARE_LIBBPF_OPTS(bpf_object_open_opts, opts2);
-	DECLARE_LIBXDP_OPTS(xdp_program_opts, xdp_opts2, 0);
-	
-	xdp_opts2.open_filename = "xdp_prog.o";
-	xdp_opts2.prog_name = "xdp_pass_func";
-	xdp_opts2.opts = &opts2;
-	
-	output_program = xdp_program__create(&xdp_opts2);
-	err = libxdp_get_error(output_program);
+	err = attach_xdp(input_ifindex, prog_fd, &input_xdp_flags);
 	if (err) {
-		libxdp_strerror(err, errmsg, sizeof(errmsg));
-		fprintf(stderr, "ERR: Failed to load XDP program 'xdp_pass_func': %s\n", errmsg);
-		err = EXIT_FAIL_BPF;
-		goto cleanup;
+		fprintf(stderr, "ERR: Failed to attach XDP program to %s\n", input_ifname);
+		bpf_object__close(bpf_obj);
+		return EXIT_FAIL_BPF;
 	}
+	printf("Attached XDP program to %s in %s mode\n", input_ifname,
+	       (input_xdp_flags & XDP_FLAGS_DRV_MODE) ? "native" : "SKB");
 	
-	/* Try native mode first, fall back to SKB mode if not supported */
-	err = xdp_program__attach(output_program, output_ifindex, XDP_MODE_NATIVE, 0);
-	if (err) {
-		/* If native mode fails, try SKB mode */
-		output_attach_mode = XDP_MODE_SKB;
-		err = xdp_program__attach(output_program, output_ifindex, output_attach_mode, 0);
-		if (err) {
-			libxdp_strerror(err, errmsg, sizeof(errmsg));
-			fprintf(stderr, "ERR: Failed to attach XDP program to output interface %s "
-				"(tried both native and SKB modes): %s\n", argv[2], errmsg);
-			xdp_program__close(output_program);
-			output_program = NULL;
-			err = EXIT_FAIL_BPF;
-			goto cleanup;
-		}
-		printf("Attached XDP program to output interface %s in SKB mode\n", argv[2]);
+	/* Load and attach pass program to output interface */
+	printf("Loading XDP pass program for output interface %s...\n", output_ifname);
+	output_obj = load_bpf_object("xdp_prog.o", "xdp_pass_func", &output_prog_fd);
+	if (!output_obj) {
+		fprintf(stderr, "WARNING: Failed to load output XDP program, redirect may not work\n");
 	} else {
-		printf("Attached XDP program to output interface %s in native mode\n", argv[2]);
-	}
-	
-	/* Initialize global stats map to ensure it exists */
-	int map_fd_global = find_map_fd(bpf_obj, "global_stats_map");
-	if (map_fd_global >= 0) {
-		__u32 zero = 0;
-		struct global_stats init_stats = {0};
-		/* Try to initialize - ignore error if already exists */
-		bpf_map_update_elem(map_fd_global, &zero, &init_stats, BPF_NOEXIST);
+		err = attach_xdp(output_ifindex, output_prog_fd, &output_xdp_flags);
+		if (err) {
+			fprintf(stderr, "WARNING: Failed to attach XDP program to %s\n", output_ifname);
+			bpf_object__close(output_obj);
+			output_obj = NULL;
+		} else {
+			printf("Attached XDP pass program to %s in %s mode\n", output_ifname,
+			       (output_xdp_flags & XDP_FLAGS_DRV_MODE) ? "native" : "SKB");
+		}
 	}
 	
 	/* Configure redirect map with output interface */
@@ -520,36 +552,46 @@ int main(int argc, char **argv)
 	/* Verify the map was set correctly */
 	unsigned int verify_ifindex = 0;
 	err = bpf_map_lookup_elem(map_fd_tx, &key, &verify_ifindex);
-	if (err || verify_ifindex != output_ifindex) {
-		fprintf(stderr, "ERR: Failed to verify tx_port map (got ifindex %u, expected %u)\n",
+	if (err || verify_ifindex != (unsigned int)output_ifindex) {
+		fprintf(stderr, "ERR: Failed to verify tx_port map (got ifindex %u, expected %d)\n",
 			verify_ifindex, output_ifindex);
 		err = EXIT_FAIL_BPF;
 		goto cleanup;
 	}
 	
-	printf("Configured redirect: %s (ifindex %d) -> %s (ifindex %u)\n",
-	       cfg.ifname, cfg.ifindex, argv[2], output_ifindex);
-	printf("Verified tx_port map: key=0 -> ifindex=%u\n", verify_ifindex);
-	printf("XDP program attached to input interface %s (ifindex %d)\n", 
-	       cfg.ifname, cfg.ifindex);
+	printf("Configured redirect: %s (ifindex %d) -> %s (ifindex %d)\n",
+	       input_ifname, input_ifindex, output_ifname, output_ifindex);
+	
+	/* Enable promiscuous mode on input interface */
+	struct ifreq ifr;
+	int sock_promisc = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock_promisc >= 0) {
+		memset(&ifr, 0, sizeof(ifr));
+		strncpy(ifr.ifr_name, input_ifname, IFNAMSIZ - 1);
+		if (ioctl(sock_promisc, SIOCGIFFLAGS, &ifr) == 0) {
+			ifr.ifr_flags |= IFF_PROMISC;
+			if (ioctl(sock_promisc, SIOCSIFFLAGS, &ifr) == 0) {
+				printf("Enabled promiscuous mode on %s\n", input_ifname);
+			}
+		}
+		close(sock_promisc);
+	}
 	
 	/* Setup AF_XDP socket for matching packets */
-	output_ifname = argv[2]; /* Store output interface name for packet reader */
-	if (setup_af_xdp_socket(cfg.ifname, cfg.ifindex, bpf_obj, argv[2]) < 0) {
-		fprintf(stderr, "WARNING: Failed to setup AF_XDP socket, matching packets will go to output interface\n");
+	if (setup_af_xdp_socket(input_ifname, input_ifindex, bpf_obj) < 0) {
+		fprintf(stderr, "WARNING: Failed to setup AF_XDP socket, matching packets will use DEVMAP redirect\n");
 	} else {
 		/* Start packet reader thread */
 		if (pthread_create(&xsk_thread, NULL, xsk_packet_reader, xsk_info) != 0) {
 			fprintf(stderr, "ERR: Failed to create AF_XDP reader thread: %s\n", strerror(errno));
-			/* Cleanup will handle xsk_info */
 		} else {
 			printf("AF_XDP packet reader thread started\n");
 		}
 	}
 	
-	printf("\nNOTE: Make sure there is traffic on %s for counters to increment.\n", cfg.ifname);
-	printf("      Matching packets will be sent to %s via AF_XDP socket.\n", argv[2]);
-	printf("      Non-matching packets will be redirected to %s.\n", argv[2]);
+	printf("\nNOTE: Make sure there is traffic on %s for counters to increment.\n", input_ifname);
+	printf("      Matching packets will be sent to %s via AF_XDP socket.\n", output_ifname);
+	printf("      Non-matching packets will be redirected to %s via DEVMAP.\n", output_ifname);
 	printf("Press Ctrl+C to stop\n\n");
 	
 	/* Set up signal handler */
@@ -564,17 +606,15 @@ int main(int argc, char **argv)
 		}
 	}
 	
-	printf("\nDetaching XDP program...\n");
+	printf("\nDetaching XDP programs...\n");
+	err = 0; /* Reset error for cleanup */
 	
 cleanup:
 	/* Wait for AF_XDP thread to finish */
 	if (xsk_info && xsk_info->xsk) {
-		/* Only join if thread was created (socket exists) */
 		pthread_join(xsk_thread, NULL);
-		if (xsk_info->xsk) {
-			xsk_socket__delete(xsk_info->xsk);
-		}
-		if (xsk_info->umem && xsk_info->umem->umem) {
+		xsk_socket__delete(xsk_info->xsk);
+		if (xsk_info->umem) {
 			xsk_umem__delete(xsk_info->umem->umem);
 			if (xsk_info->umem->buffer) {
 				munmap(xsk_info->umem->buffer, NUM_FRAMES * FRAME_SIZE);
@@ -584,21 +624,22 @@ cleanup:
 		free(xsk_info);
 	}
 	
-	if (output_program) {
-		int detach_err = xdp_program__detach(output_program, output_ifindex, output_attach_mode, 0);
+	/* Detach output XDP program */
+	if (output_obj) {
+		int detach_err = detach_xdp(output_ifindex, output_xdp_flags);
 		if (detach_err) {
-			libxdp_strerror(detach_err, errmsg, sizeof(errmsg));
-			fprintf(stderr, "Error detaching output XDP program: %s\n", errmsg);
+			fprintf(stderr, "Error detaching output XDP program: %s\n", strerror(-detach_err));
 		}
-		xdp_program__close(output_program);
+		bpf_object__close(output_obj);
 	}
-	if (program) {
-		int detach_err = xdp_program__detach(program, cfg.ifindex, cfg.attach_mode, 0);
+	
+	/* Detach input XDP program */
+	if (bpf_obj) {
+		int detach_err = detach_xdp(input_ifindex, input_xdp_flags);
 		if (detach_err) {
-			libxdp_strerror(detach_err, errmsg, sizeof(errmsg));
-			fprintf(stderr, "Error detaching input XDP program: %s\n", errmsg);
+			fprintf(stderr, "Error detaching input XDP program: %s\n", strerror(-detach_err));
 		}
-		xdp_program__close(program);
+		bpf_object__close(bpf_obj);
 	}
 	
 	return err == 0 ? EXIT_OK : EXIT_FAIL;
